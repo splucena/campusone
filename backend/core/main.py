5156c95829920a0b776e9545ae12aac58742a46b
Sumilang Plucena <splucena2011@gmail.com>
2025-02-02 15:27:51 +0800

    Containerize backend core app

--- backend/core/main.py
+++ backend/core/main.py
@@ -6,4 +6,9 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello world!"}
+    return {"message": "Hello world!!!x"}
+
+
+@app.get("/dream-big")
+async def dream_big():
+    return {"message": "From core-web-app Dream BIG!!!"}
